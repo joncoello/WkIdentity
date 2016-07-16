d282fd58fa7d06a2a37227f041e58fdb1887b586
jonco <jonco@DESKTOP-360ND21>
2016-07-16 10:41:14 +0100

    add scopes

---
+++ WkIdentity/Models/Scopes.cs
@@ -0,0 +1,19 @@
+﻿using IdentityServer3.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WkIdentity.Models {
+    static class Scopes {
+        public static List<Scope> Get() {
+            return new List<Scope>
+            {
+            new Scope
+            {
+                Name = "api1"
+            }
+        };
+        }
+    }
+}
