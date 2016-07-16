b0cf80080c3920b4d14a94ecd1378aa97f0bdf14
jonco <jonco@DESKTOP-360ND21>
2016-07-16 14:06:39 +0100

    add resource serve controller and config

---
+++ WkResource/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace WkResource.Controllers
+{
+    [Route("test")]
+    public class TestController : ApiController {
+        public IHttpActionResult Get() {
+            var caller = User as ClaimsPrincipal;
+
+            return Json(new {
+                message = "OK computer",
+                client = caller.FindFirst("client_id").Value
+            });
+        }
+    }
+}
---
+++ WkResource/Startup.cs
@@ -0,0 +1,33 @@
+﻿using IdentityServer3.AccessTokenValidation;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WkResource {
+
+    public class Startup {
+
+        public void Configuration(IAppBuilder app) {
+            // accept access tokens from identityserver and require a scope of 'api1'
+            app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions {
+                Authority = "http://wkidentity.azurewebsites.net/",
+                ValidationMode = ValidationMode.ValidationEndpoint,
+
+                RequiredScopes = new[] { "api1" }
+            });
+
+            // configure web api
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+
+            // require authentication for all controllers
+            config.Filters.Add(new AuthorizeAttribute());
+
+            app.UseWebApi(config);
+        }
+
+    }
+}
