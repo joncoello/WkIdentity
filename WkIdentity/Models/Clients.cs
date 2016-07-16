479a1e47631f68450526cbcc969f2946b936d4d4
jonco <jonco@DESKTOP-360ND21>
2016-07-16 10:42:16 +0100

    add clients

---
+++ WkIdentity/Models/Clients.cs
@@ -0,0 +1,35 @@
+﻿using IdentityServer3.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WkIdentity.Models {
+    static class Clients {
+        public static List<Client> Get() {
+            return new List<Client>
+            {
+           // no human involved
+            new Client
+            {
+                ClientName = "Silicon-only Client",
+                ClientId = "silicon",
+                Enabled = true,
+                AccessTokenType = AccessTokenType.Reference,
+
+                Flow = Flows.ClientCredentials,
+
+                ClientSecrets = new List<Secret>
+                {
+                    new Secret("F621F470-9731-4A25-80EF-67A6F7C5F4B8".Sha256())
+                },
+
+                AllowedScopes = new List<string>
+                {
+                    "api1"
+                }
+            }
+        };
+        }
+    }
+}
