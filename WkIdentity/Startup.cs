e7591013bf9ae891da8bb553c299c34577cefa7f
jonco <jonco@DESKTOP-360ND21>
2016-07-16 13:35:45 +0100

    add owin and identify config

---
+++ WkIdentity/Startup.cs
@@ -0,0 +1,27 @@
+﻿using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Services.InMemory;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WkIdentity.Models;
+
+namespace WkIdentity {
+
+    public class Startup {
+
+        public void Configuration(IAppBuilder app) {
+            var options = new IdentityServerOptions {
+                Factory = new IdentityServerServiceFactory()
+                            .UseInMemoryClients(Clients.Get())
+                            .UseInMemoryScopes(Scopes.Get())
+                            .UseInMemoryUsers(new List<InMemoryUser>()),
+
+                RequireSsl = false
+            };
+
+            app.UseIdentityServer(options);
+        }
+    }
+}
